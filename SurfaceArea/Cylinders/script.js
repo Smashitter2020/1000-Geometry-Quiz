99e1991e7d802365fab808822f3ce89bbf81641f
Smashitter2020 <smashitter2020@gmail.com>
2025-07-31 16:48:25 -0700

    Create script.js

---
+++ SurfaceArea/Cylinders/script.js
@@ -0,0 +1,35 @@
+let radius, height;
+let score = 0;
+let currentQuestion = 1;
+let maxQuestions = 1000;
+
+function generateQuestion() {
+  radius = Math.floor(Math.random() * 20) + 1;
+  height = Math.floor(Math.random() * 20) + 1;
+
+  document.getElementById("question").innerHTML = `<math xmlns="http://www.w3.org/1998/Math/MathML"><mi>r</mi><mo>=</mo><mn>${radius}</mn><mo>,</mo><mi>h</mi><mo>=</mo><mn>${height}</mn></math>`;
+  document.getElementById("answer").value = "";
+  document.getElementById("currentQuestion").innerText = currentQuestion + " out of " + maxQuestions + " questions";
+}
+
+function checkAnswer() {
+  const userAnswer = parseFloat(document.getElementById("answer").value);
+  const correctAnswer = 2 * Math.PI * radius * (height + radius);
+  currentQuestion++;
+
+  if (Math.abs(userAnswer - correctAnswer) < 0.01) {
+    score++;
+  }
+
+  generateQuestion();
+  if (currentQuestion > maxQuestions) {
+    document.getElementById("submitBtn").classList.add("d-none");
+    document.getElementById("question").classList.add("d-none");
+    document.getElementById("answer").classList.add("d-none");
+    document.getElementById("currentQuestion").classList.add("d-none");
+    document.getElementById("finalScore").classList.remove("d-none");
+    document.getElementById("finalScore").innerText = "Final score: " + score + "/" + maxQuestions + " (" + (Math.round((score * 100) / maxQuestions)) + "%)";
+  }
+}
+
+window.onload = generateQuestion;
